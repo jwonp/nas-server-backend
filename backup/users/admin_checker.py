e3b8002cd675280c5f714ae33a4a82f571d8d62b
Joowon <201611804@kyonggi.ac.kr>
2023-01-07 21:25:31 +0900

    delete backup .venv .vscode

--- backup/users/admin_checker.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth import authenticate
-u = authenticate(username="prodge", password="1234")
-print(u.is_staff)
-print(u.is_superuser)
-print(u.is_active)
--- backup/users/functions.py
+++
@@ -1,6 +0,0 @@
-from .models import UserData
-from .serializers import UserSerializer
-def get_user_id(username):
-    user_data = UserData.objects.get(id=username)
-    serializer = UserSerializer(user_data, many=False)
-    return serializer.data
