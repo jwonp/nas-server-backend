f3aebfe826b75e3388c03793d6c584f8812b0ae7
Joowon <201611804@kyonggi.ac.kr>
2023-02-20 17:21:53 +0900

    test

--- server_config/server_config/urls.py
+++ server_config/server_config/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from django.conf.urls import  include
 from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('users/',include('users.urls')),
     path('', include('test_rest_api.urls')),
 ]
