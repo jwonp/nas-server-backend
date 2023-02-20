da2dbb05a6cdcc174ac47468efac45f1c95d6a0e
Joowon <201611804@kyonggi.ac.kr>
2023-02-20 22:11:03 +0900

    test

--- server_config/users/views.py
+++ server_config/users/views.py
@@ -30,7 +30,7 @@ from oauth2_provider.models import get_access_token_model, get_application_model
 from oauth2_provider.signals import app_authorized
 class RefreshToken(TokenView):
     def post(self, request, *args, **kwargs):
-        return HttpResponse(request.COOKIES['refresh'])
+        return HttpResponse(json.dumps(request.COOKIES))
     # @method_decorator(sensitive_post_parameters("password"))
     # def post(self, request, *args, **kwargs):
     #     body = request.body.replace("&RefreshToken;",request.COOKIES['refresh'])
