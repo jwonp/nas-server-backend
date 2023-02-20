991b0909b8cac6a25702f7ad97ebe8204c3a3955
Joowon <201611804@kyonggi.ac.kr>
2023-02-20 15:14:53 +0900

    test debug tool

--- server_config/server_config/urls.py
+++ server_config/server_config/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/',include('users.urls')),
-    path('', include('test_rest_api.urls')),
     #debug
     path('__debug__/', include('debug_toolbar.urls')),
+    path('', include('test_rest_api.urls')),
 ]
