39745cad05ca8f5df176f5fee9d628f2a0917e48
Joowon <201611804@kyonggi.ac.kr>
2023-02-23 02:26:31 +0900

    dubug off

--- server_config/server_config/settings.py
+++ server_config/server_config/settings.py
@@ -6,10 +6,10 @@ from corsheaders.defaults import default_headers
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG=True
+DEBUG=False
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-env = environ.Env(DEBUG=(bool,True))
+env = environ.Env(DEBUG=(bool,False))
 environ.Env.read_env(
     env_file = os.path.join(BASE_DIR,'.env')
 )
