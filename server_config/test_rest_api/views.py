4cd064cba510699cba788a07cb43203e2193b7ff
Joowon <201611804@kyonggi.ac.kr>
2023-02-22 16:43:12 +0900

    test

--- server_config/test_rest_api/views.py
+++ server_config/test_rest_api/views.py
@@ -1,8 +1,12 @@
 from django.http.response import HttpResponse
+from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.contrib.auth import authenticate,login as run_login, logout as run_logout
 from .functions import save_user,save_user_storage
+
+def logout_page(request):
+    return render(request, 'test_rest_api/logout.html')
 
 @api_view(['POST'])
 def getFolders(request):
