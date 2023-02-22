c9a67375aa0a8ebd3da2b8f7d777bbe0806ccea6
Joowon <201611804@kyonggi.ac.kr>
2023-02-22 16:37:54 +0900

    test

--- server_config/test_rest_api/urls.py
+++ server_config/test_rest_api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-
-
 from . import views
+app_name = 'test_rest_api'
 urlpatterns = [
     path('getfolders/', views.getFolders, name='getfolders'),
     path('logout/', views.logout, name='logout'),
