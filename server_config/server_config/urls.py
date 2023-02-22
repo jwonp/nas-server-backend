41896bc064031b5d6f2262c1c858ffcb7ad7c572
Joowon <201611804@kyonggi.ac.kr>
2023-02-22 23:08:56 +0900

    test logout

--- server_config/server_config/urls.py
+++ server_config/server_config/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import  include
 from django.contrib.auth import views as auth_views
 urlpatterns = [
   
+    path('admin/logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'), #new
     path('admin/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'), #new
     path('admin/', admin.site.urls),
     path('users/',include('users.urls')),
