634838d5dce7a44868cfa2f2a172e1fb01e4f907
Joowon <201611804@kyonggi.ac.kr>
2023-02-28 00:09:08 +0900

    test admin page

--- server_config/users/views.py
+++ server_config/users/views.py
@@ -61,7 +61,7 @@ class get_all_data(ProtectedResourceView):
             'stoarages': storages()
         }
         serializer = switch.get(key)
-        return HttpResponse(data=serializer.data) 
+        return HttpResponse(serializer.data) 
     
  ######   
 
