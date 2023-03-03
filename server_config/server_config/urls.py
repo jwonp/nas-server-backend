7367b7e037e359f246f106bacf9fc435593c612d
Joowon <201611804@kyonggi.ac.kr>
2023-03-03 11:07:57 +0900

    set Debug false

--- server_config/server_config/urls.py
+++ server_config/server_config/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'), #new
     path('admin/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'), #new
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('users/',include('users.urls')),
     path('', include('test_rest_api.urls')),
 ]
