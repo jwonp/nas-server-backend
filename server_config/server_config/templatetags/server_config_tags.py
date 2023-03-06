28e5f74b068ed666b6b2cb175668b659eb974ceb
Joowon <201611804@kyonggi.ac.kr>
2023-03-07 02:04:19 +0900

    test template tag

--- server_config/server_config/templatetags/server_config_tags.py
+++ server_config/server_config/templatetags/server_config_tags.py
@@ -1,9 +1,9 @@
 import os
 from django import template
-
+from django.conf import settings
 register = template.Library()
 
 
 @register.simple_tag
 def get_env_var(key):
-    return os.environ.get(key)
+    return settings.AUTH_DATA.get(key)
