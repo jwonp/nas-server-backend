b81f0d662f58e7c2fd126d31c2e0011d18042081
Joowon <201611804@kyonggi.ac.kr>
2023-03-07 01:56:37 +0900

    test logout

--- server_config/server_config/urls.py
+++ server_config/server_config/urls.py
@@ -1,12 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
 from django.conf.urls import include
 from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('admin/logout/', auth_views.LogoutView.as_view(template_name='logout.html',
-         context={'CODE_CHALLENGE': settings.AUTH_DATA.get("CODE_CHALLENGE"), "CLIENT_ID": settings.AUTH_DATA.get('CLIENT_ID')}), name='logout'),  # new
+    path('admin/logout/', auth_views.LogoutView.as_view(template_name='logout.html'),
+         name='logout'),  # new
     path('admin/login/', auth_views.LoginView.as_view(template_name='login.html'),
          name='login'),  # new
     path('admin/', admin.site.urls),
